40ba8a11213d937d1e3e26813196fafc2ad145e7
Niklas Liesch <niklas.liesch@protonmail.com>
2025-02-17 17:18:39 +0100

    adding primitive solve capabilities 	modified:   src/mat3x3.rs

--- src/mat3x3.rs
+++ src/mat3x3.rs
@@ -1,4 +1,7 @@
-use std::ops::{Add, Div, Mul, Sub};
+use std::{
+    fmt::Debug,
+    ops::{Add, Div, Mul, Neg, Sub},
+};
 
 #[derive(Debug, Clone, Copy, Default, PartialEq)]
 pub struct Matrix3x3<F>
@@ -9,7 +12,8 @@ where
         + Sub<Output = F>
         + Copy
         + PartialEq
-        + Default,
+        + Default
+        + Neg<Output = F>,
 {
     m: [[F; 3]; 3],
 }
@@ -22,7 +26,8 @@ where
         + Sub<Output = F>
         + Copy
         + PartialEq
-        + Default,
+        + Default
+        + Neg<Output = F>,
 {
     type Output = Self;
 
@@ -45,7 +50,8 @@ where
         + Sub<Output = F>
         + Copy
         + PartialEq
-        + Default,
+        + Default
+        + Neg<Output = F>,
 {
     type Output = Self;
 
@@ -67,7 +73,8 @@ where
         + Sub<Output = F>
         + Copy
         + PartialEq
-        + Default,
+        + Default
+        + Neg<Output = F>,
 {
     type Output = Self;
 
@@ -93,7 +100,8 @@ where
         + Sub<Output = F>
         + Copy
         + PartialEq
-        + Default,
+        + Default
+        + Neg<Output = F>,
 {
     type Output = Self;
 
@@ -116,7 +124,8 @@ where
         + Sub<Output = F>
         + Copy
         + PartialEq
-        + Default,
+        + Default
+        + Neg<Output = F>,
 {
     pub fn unit_matrix() -> Self
     where
@@ -139,10 +148,13 @@ where
             - self.m[0][1] * self.m[1][0] * self.m[2][2]
             - self.m[0][0] * self.m[1][2] * self.m[2][1]
     }
-    fn add_row(&self, i: usize, row: [F; 3]) -> Self {
-        let mut t = self.m;
-        t[i] = [t[i][0] + row[0], t[i][1] + row[1], t[i][2] + row[2]];
-        Matrix3x3 { m: t }
+
+    fn add_row(&mut self, i: usize, row: [F; 3]) {
+        self.m[i] = [
+            self.m[i][0] + row[0],
+            self.m[i][1] + row[1],
+            self.m[i][2] + row[2],
+        ];
     }
 
     fn mul_row(&self, i: usize, factor: F) -> [F; 3] {
@@ -151,6 +163,54 @@ where
             self.m[i][1] * factor,
             self.m[i][2] * factor,
         ]
+    }
+
+    //this is cancer, don't try to understand it, you will fail!
+    fn solve(&self, mut b: [F; 3]) -> Option<[F; 3]>
+    where
+        F: From<u8>,
+    {
+        if self.determinant() == 0.into() {
+            return None;
+        }
+        let mut tmp = self.to_owned();
+
+        //create zeros in the first column
+        //second row
+        let mut k = -(tmp.m[1][0] / tmp.m[0][0]);
+        tmp.add_row(1, tmp.mul_row(0, k));
+        b[1] = b[1] + k * b[0];
+
+        //third row
+        k = -(tmp.m[2][0] / tmp.m[0][0]);
+        tmp.add_row(2, tmp.mul_row(0, k));
+        b[2] = b[2] + k * b[0];
+
+        //create zero in second column
+        k = -(tmp.m[2][1] / tmp.m[1][1]);
+        tmp.add_row(2, tmp.mul_row(1, k));
+        b[2] = b[2] + k * b[1];
+
+        //create the 1 in bottom right
+        b[2] = b[2] / tmp.m[2][2];
+        tmp.m[2][2] = tmp.m[2][2] / tmp.m[2][2];
+
+        //zeros in 3rd column
+        b[1] = b[1] - tmp.m[1][2] * b[2];
+        tmp.add_row(1, tmp.mul_row(2, -tmp.m[1][2]));
+
+        b[0] = b[0] - tmp.m[0][2] * b[2];
+        tmp.add_row(0, tmp.mul_row(2, -tmp.m[0][2]));
+
+        //create 1 in center
+        b[1] = b[1] / tmp.m[1][1];
+        tmp.m[1][1] = tmp.m[1][1] / tmp.m[1][1];
+
+        //zero in second column
+        b[0] = b[0] - tmp.m[0][1] * b[1];
+        tmp.add_row(0, tmp.mul_row(1, -tmp.m[0][1]));
+
+        Some(b)
     }
 }
 
@@ -166,5 +226,25 @@ mod test {
         };
         assert_eq!(m2, m1 * m2);
         assert_eq!(m2, m2 * m1);
+    }
+
+    #[test]
+    fn solve_lin_eq() {
+        let m2: Matrix3x3<f32> = Matrix3x3 {
+            m: [[1., 1., 1.], [0., 1., 1.], [1., 0., -1.]],
+        };
+        let v = [2., 2., 2.];
+        let s = [0., 4., -2.];
+        assert_eq!(s, m2.solve(v).unwrap());
+    }
+
+    #[test]
+    fn solve_lin_eq_inf() {
+        let m2: Matrix3x3<f32> = Matrix3x3 {
+            m: [[1., 1., 1.], [0., 1., 1.], [0., 0., 0.]],
+        };
+        let v = [2., 2., 2.];
+        let s = [0., 4., -2.];
+        assert_eq!(None, m2.solve(v));
     }
 }
